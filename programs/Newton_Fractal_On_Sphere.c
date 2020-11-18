c5330bc7bc366b47cdfe32ef51a35716b65d5f2d
ryanmaguire <rmaguire@protonmail.com>
2020-11-18 18:35:45 -0500

    Updating algorithm for Newton-Raphson fractal.

--- programs/Newton_Fractal_On_Sphere.c
+++ programs/Newton_Fractal_On_Sphere.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <complex.h>
 
@@ -11,53 +12,227 @@ typedef struct three_vec {
     double dat[3];
 } three_vec;
 
+typedef struct root_struct {
+    complex double *roots;
+    unsigned int n_roots;
+} root_struct;
+
 /******************************************************************************
  ******************************************************************************
  *                          Begin User Input                                  *
  ******************************************************************************
  ******************************************************************************/
 
-/*  The roots of z^3 - 1.                                                     */
-const complex double ROOT_1 = 1.0;
-const complex double ROOT_2 = -0.5 + 0.8660254037844386*_Complex_I;
-const complex double ROOT_3 = -0.5 - 0.8660254037844386*_Complex_I;
+/*  The number of pixels in the x and y axes.                                 */
+const int size = 4*1024;
 
-/*  Create a variable for the number of roots. z^3-1 has three.               */
-#define NRoots 3
-
-/* List the roots of z^3 - 1.                                                 */
-complex double roots[NRoots] = {ROOT_1, ROOT_2, ROOT_3};
+/*  Maximum number of iterations for the Newton-Raphson method. This must be  *
+ *  less than 255, otherwise we'll run out of colors.                         */
+const unsigned char MaxIters = 32;
 
 /*  Define the camera angle for the image of the sphere. This is where the    *
  *  observer looking at the sphere will be placed. The output image is        *
  *  normalized to take up the entire screen, so u and lambda*u will have the  *
  *  same result for any positive lambda. u and -u will have different results.*/
-three_vec camera_pos = {{0.0, 0.0, 1.0}};
+three_vec camera_pos = {{1.0, 1.0, 1.0}};
 
-/*  Define the polynomial z^3 - 1 and it's derivative. You can change this    *
- *  polynomial to whatever you want, but you will need to adjust the roots.   */
-static complex double f(complex double z)
-{
-    return z*z*z - 1.0;
-}
+/*  The degree of the polynomial.                                             */
+#define deg 3
 
-static complex double f_prime(complex double z)
-{
-    return 3.0*z*z;
-}
-
-/*  The colors used for drawing. We're setting this as red, green, blue. If   *
- *  you have a polynomial with more than three roots, you'll need more than   *
- *  three colors.                                                             */
-unsigned char colors[NRoots][3] = {
-    {255, 0, 30}, {0, 255, 30}, {0, 30, 255}
-};
+/*  The coefficients of the polynomial. The zeroth coefficient is for z^deg   *
+ *  and the last coefficient is the constant term.                            */
+complex double coeffs[deg+1] = {1, 0, 0, -1};
 
 /******************************************************************************
  ******************************************************************************
  *                            End User Input                                  *
  ******************************************************************************
  ******************************************************************************/
+
+#define N_COLORS 10
+const double PI = 3.14159265358979323846264338327950288419716;
+
+/*  Function for setting colors that can be used in a drawing.                */
+static unsigned char **get_colors(void)
+{
+    unsigned int n;
+    unsigned char **colors = malloc(sizeof(*colors) * N_COLORS);
+
+    for (n=0; n<N_COLORS; ++n)
+        colors[n] = malloc(sizeof(*colors[n]) * 3);
+
+    /*  Red.    */
+    colors[0][0] = (unsigned char)255;
+    colors[0][1] = (unsigned char)0;
+    colors[0][2] = (unsigned char)30;
+
+    /*  Green.  */
+    colors[1][0] = (unsigned char)0;
+    colors[1][1] = (unsigned char)255;
+    colors[1][2] = (unsigned char)30;
+
+    /*  Blue.   */
+    colors[2][0] = (unsigned char)0;
+    colors[2][1] = (unsigned char)30;
+    colors[2][2] = (unsigned char)255;
+
+    /*  Yellow. */
+    colors[3][0] = (unsigned char)255;
+    colors[3][1] = (unsigned char)255;
+    colors[3][2] = (unsigned char)51;
+
+    /*  Light Blue. */
+    colors[4][0] = (unsigned char)128;
+    colors[4][1] = (unsigned char)212;
+    colors[4][2] = (unsigned char)255;
+
+    /*  Magenta.    */
+    colors[5][0] = (unsigned char)255;
+    colors[5][1] = (unsigned char)29;
+    colors[5][2] = (unsigned char)206;
+
+    /*  Teal.   */
+    colors[6][0] = (unsigned char)0;
+    colors[6][1] = (unsigned char)128;
+    colors[6][2] = (unsigned char)128;
+
+    /*  Purple. */
+    colors[7][0] = (unsigned char)255;
+    colors[7][1] = (unsigned char)0;
+    colors[7][2] = (unsigned char)255;
+
+    /*  Orange. */
+    colors[8][0] = (unsigned char)255;
+    colors[8][1] = (unsigned char)85;
+    colors[8][2] = (unsigned char)0;
+
+    /*  Champagne. */
+    colors[9][0] = (unsigned char)255;
+    colors[9][1] = (unsigned char)85;
+    colors[9][2] = (unsigned char)0;
+
+    return colors;
+}
+
+static void destroy_colors(unsigned char **colors)
+{
+    unsigned int n;
+    for (n=0; n<N_COLORS; ++n)
+        free(colors[n]);
+
+    free(colors);
+}
+
+/*  Define the polynomial based on the user provided coefficients. Compute    *
+ *  this via Horner's method for speed.                                       */
+static complex double f(complex double z)
+{
+    complex double out;
+    int n;
+
+    out = coeffs[0];
+    for (n=1; n<=deg; ++n)
+        out = z*out + coeffs[n];
+
+    return out;
+}
+
+static complex double f_prime(complex double z)
+{
+    complex double out;
+    int n;
+
+    out = deg*coeffs[0];
+    for (n=1; n<deg; ++n)
+        out = z*out + deg*coeffs[n];
+
+    return out;
+}
+
+static root_struct *get_roots(void)
+{
+    root_struct *out = malloc(sizeof(*out));
+    complex double p, root;
+    unsigned int m, n, ell, iter, max_iter, s, N, n_roots;
+    double r, theta, factor_1, factor_2, min, temp;
+
+    out->roots = malloc(sizeof(*out->roots) * deg);
+
+    s = (int)ceil(0.26632*log(deg));
+    N = (int)ceil(8.32547*deg*log(deg));
+    n_roots = 0;
+    max_iter = 20;
+
+    factor_1 = 1.0+sqrt(2);
+    factor_2 = (deg-1.0)/deg;
+
+    for (m=0; m<s; ++m)
+    {
+        if (n_roots >= deg)
+            break;
+
+        r = factor_1 * pow(factor_2, (2*m+1)/(4*s));
+
+        for (n=0; n<N; ++n)
+        {
+            if (n_roots >= deg)
+                break;
+
+            theta = 2*PI*n/N;
+            p = r * (cos(theta) + _Complex_I*sin(theta));
+
+            for (iter=0; iter<max_iter; ++iter)
+            {
+                root = p - f(p)/f_prime(p);
+                if (cabs(root - p) < 1.0e-10)
+                    break;
+
+                p = root;
+            }
+
+            if (cabs(f(root)) < 1.0e-8)
+            {
+                if (n_roots == 0)
+                {
+                    n_roots += 1;
+                    out->roots[0] = root;
+                }
+                else
+                {
+                    min = cabs(root - out->roots[0]);
+                    for (ell=1; ell<n_roots; ++ell)
+                    {
+                        temp = cabs(root - out->roots[ell]);
+                        if (temp < min)
+                            min = temp;
+                    }
+                    if (min >= 0.0001)
+                    {
+                        out->roots[n_roots] = root;
+                        n_roots += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    out->n_roots = n_roots;
+
+    if (n_roots == 0)
+    {
+        puts("Error:");
+        puts("\tFailed to find the roots. Aborting.");
+        exit(0);
+    }
+
+    return out;
+}
+
+static void destroy_roots(root_struct *the_roots)
+{
+    free(the_roots->roots);
+    free(the_roots);
+}
 
 static three_vec normalize_three_vec(three_vec p)
 {
@@ -180,6 +355,15 @@ int main(void)
     FILE *fp;
     fp = fopen("newton_cubic_fractal_on_sphere.ppm", "w");
 
+    /*  Struct for the roots.                                                 */
+    root_struct *roots_of_f = get_roots();
+
+    unsigned int n_roots = roots_of_f->n_roots;
+    complex double *roots = roots_of_f->roots;
+
+    /*  The colors for the drawing.                                           */
+    unsigned char **colors = get_colors();
+
     /*  Values for the min and max of the x and y axes.                       */
     const double x_min = -1.0;
     const double x_max =  1.0;
@@ -190,19 +374,15 @@ int main(void)
     three_vec p;
     two_vec Z, proj_p;
 
-    /*  The number of pixels in the x and y axes.                             */
-    int size  = 2*1024;
-
-    /* MaxIters should be less than 256, or we'll run out of colors.          */
-    unsigned char MaxIters = 32;
-    unsigned char factor   = 255/MaxIters;
+    /*  Factor for giving the image a gradient.                               */
+    unsigned char factor = 255/MaxIters;
 
     /* Dummy variables to loop over.                                          */
-    int iters, ind, n;
+    unsigned int iters, ind, n;
 
     /*  More dummy variables to loop over.                                    */
     int x, y;
-    double z_x, z_y, min;
+    double z_x, z_y, min, temp, scale;
     complex double z, root;
 
     fprintf(fp, "P6\n%d %d\n255\n", size, size);
@@ -234,10 +414,10 @@ int main(void)
                     proj_p = stereographic_projection(p);
                     z = proj_p.dat[0] + _Complex_I*proj_p.dat[1];
 
-                    /*  Allow iters number of iterations for Newton-Raphson.  */
+                    /*  Allow MaxIters number of iterations of Newton-Raphson.*/
                     for (iters=0; iters<MaxIters; ++iters)
                     {
-                        /*  Perfrom Newton-Raphson on z^3 - 1.                */
+                        /*  Perfrom Newton-Raphson on the polynomial f.       */
                         root = z - f(z)/f_prime(z);
 
                         /*  Checks for convergence.                           */
@@ -251,28 +431,37 @@ int main(void)
                     min = cabs(z-roots[0]);
                     ind = 0;
 
-                    for (n=1; n<NRoots; ++n)
+                    for (n=1; n<n_roots; ++n)
                     {
-                        if (cabs(z - roots[n]) < min)
+                        temp = cabs(z - roots[n]);
+                        if (temp < min)
                         {
-                            min = cabs(z-roots[n]);
+                            min = temp;
                             ind = n;
                         }
                     }
 
-                    for (n=0; n<3; ++n)
-                        brightness[n] = colors[ind][n];
+                    if (min > 0.1)
+                        color((char)0, (char)0, (char)0, fp);
+                    else
+                    {
+                        scale = (255.0-factor*iters)/255.0;
+                        for (n=0; n<3; ++n)
+                            brightness[n] =
+                                (unsigned char)(scale * colors[ind][n]);
 
-                    /*  Create a gradient to emphasize rate of convergence.   */
-                    brightness[ind] -= (unsigned char)factor*iters;
-
-                    /*  Color the current pixel.                              */
-                    color(brightness[0], brightness[1], brightness[2], fp);
+                        /*  Color the current pixel.                          */
+                        color(brightness[0], brightness[1], brightness[2], fp);
+                    }
                 }
             }
             else
                 color((char)0, (char)0, (char)0, fp);
         }
     }
+
+    /*  Free the memory allocated to colors before returning.                 */
+    destroy_colors(colors);
+    destroy_roots(roots_of_f);
     return 0;
 }
