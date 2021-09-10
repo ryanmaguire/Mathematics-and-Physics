e32732d454eb14559a33b3578ca67bb825e9d699
ryanmaguire <rmaguire@protonmail.com>
2021-09-10 19:10:26 -0400

    Messing around with Halley fractal.

--- programs/Halley_Fractal_Raster.c
+++ programs/Halley_Fractal_Raster.c
@@ -1,5 +1,47 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of Mathematics-and-Physics.                             *
+ *                                                                            *
+ *  Mathematics-and-Physics is free software: you can redistribute it and/or  *
+ *  modify it under the terms of the GNU General Public License as published  *
+ *  by the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  Mathematics-and-Physics is distributed in the hope that it will be useful *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with Mathematics-and-Physics.  If not, see                          *
+ *  <https://www.gnu.org/licenses/>.                                          *
+ ******************************************************************************
+ *  Author: Ryan Maguire                                                      *
+ *  Date:   2021/02/13                                                        *
+ ******************************************************************************
+ *  This code creates a Halley fractal for any polynomial up to degree 14.    *
+ *  If you add more colors, you can have a higher degree. It works based on   *
+ *  an augmentation of the ideas described in "How to find all roots of       *
+ *  complex polynomials by Newton's method" by John Hubbard, Dierk Schleicher,*
+ *  and Scott Sutherland. Rather than using Newton's method, Halley's is      *
+ *  implemented. The idea still works in practice, though I don't know of any *
+ *  paper proving such methods should work. This code has been greatly        *
+ *  generalized and made C89/C90 compliant in my other project, libtmpl. The  *
+ *  code presented here requires C99 features, in particular complex.h.       *
+ * 
+ *  NOTE:                                                                     *
+ *      As of this writing, Microsoft's C library does not support C99        *
+ *      features, and does not implement complex numbers according to the     *
+ *      standard. Because of this, this file will not compile on windows      *
+ *      unless you use a different C library.                                 *
+ ******************************************************************************/
+
+/*  fprintf and fputc are found here.                                         */
 #include <stdio.h>
 #include <math.h>
+
+/*  Complex numbers found here.                                               */
 #include <complex.h>
 
 /******************************************************************************
@@ -11,17 +53,9 @@
 /*  The number of pixels in the x and y axes.                                 */
 static const int size = 1024;
 
-/*  Maximum number of iterations for the Newton-Raphson method. This must be  *
- *  less than 255, otherwise we'll run out of colors.                         */
-static const unsigned int MaxIters = 32;
-
-/*  Maximum number of iterations allowed before giving up on the root finding *
- *  algorithm. If no roots are found, the computation aborts.                 */
-static const unsigned int root_finder_max_iter = 200;
-
 /*  The coefficients of the polynomial. The zeroth coefficient is for z^deg   *
  *  and the last coefficient is the constant term.                            */
-static const complex double coeffs[] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1};
+static const complex double coeffs[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 /******************************************************************************
  ******************************************************************************
@@ -29,142 +63,206 @@ static const complex double coeffs[] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1};
  ******************************************************************************
  ******************************************************************************/
 
-static const unsigned int DEG = sizeof(coeffs)/sizeof(coeffs[0]) - 1U;
+/*  Maximum number of iterations for the Newton-Raphson method. This must be  *
+ *  less than 255, otherwise we'll run out of colors.                         */
+static const unsigned int max_iters = 64;
+
+/*  Maximum number of iterations allowed before giving up on the root finding *
+ *  algorithm. If no roots are found, the computation aborts.                 */
+static const unsigned int root_finder_max_iter = 200;
+
+/*  The degree of the polynomial the user selected.                           */
+#define DEG (sizeof(coeffs)/sizeof(coeffs[0]) - 1U)
+
+/*  A constant for pi. Some implementations include pi as M_PI, but we'll     *
+ *  define it here.                                                           */
 static const double PI = 3.14159265358979323846264338327950288419716;
 
+/*  Struct for dealing with colors in RGB format.                             */
 struct color {
     unsigned char red;
     unsigned char green;
     unsigned char blue;
 };
 
-/*  Define the polynomial based on the user provided coefficients. Compute    *
- *  this via Horner's method for speed.                                       */
+/*  Evaluates the user-specified polynomial at a complex number.              */
 static complex double poly_eval(complex double z)
 {
-    complex double out;
+    /*  Variable for indexing.                                                */
     unsigned int n;
 
-    out = coeffs[0];
+    /*  Initialize the output to a_{0}.                                       */
+    complex double out = coeffs[0];
+
+    /*  Compute the polynomial via Horner's method.                           */
     for (n = 1U; n <= DEG; ++n)
         out = z*out + coeffs[n];
 
     return out;
 }
+/*  End of poly_eval.                                                         */
 
+/*  Evaluates the derivative of the user-specified polynomial at a complex    *
+ *  number.                                                                   */
 static complex double poly_prime_eval(complex double z)
 {
-    complex double out;
+    /*  Variable for indexing.                                                */
     unsigned int n;
 
-    out = DEG*coeffs[0];
+    /*  Initialize the output to deg * a_{0}.                                 */
+    complex double out = DEG*coeffs[0];
+
+    /*  Compute the derivative of the polynomial via Horner's method.         */
     for (n = 1U; n < DEG; ++n)
         out = z*out + DEG*coeffs[n];
 
     return out;
 }
+/*  End of poly_prime_eval.                                                   */
 
+/*  Computes the second derivative of the polynomial.                         */
 static complex double poly_double_prime_eval(complex double z)
 {
-    complex double out;
+    /*  Variable for indexing.                                                */
     unsigned int n;
 
-    out = DEG*(DEG-1)*coeffs[0];
+    /*  Initialize the output to deg * (deg-1) * a_{0}.                       */
+    complex double out = DEG*(DEG-1U)*coeffs[0];
+
+    /*  Compute the second derivative of the polynomial via Horner's method.  */
     for (n = 1U; n < DEG - 1U; ++n)
         out = z*out + DEG*(DEG-1)*coeffs[n];
 
     return out;
 }
+/*  End of poly_double_prime_eval.                                            */
 
+/*  Computes all of the roots of the input polynomial. The integer it returns *
+ *  is the number of roots. The roots are stored in the pointer roots.        */
 static unsigned int get_roots(unsigned int deg, complex double *roots)
 {
-    complex double p, root, f_p, df_p, d2f_p;
-    unsigned int m, n, ell, iter, s, N, n_roots;
-    double r, theta, factor_1, factor_2, min, temp, N_real, s_real;
+    complex double p, f_p, df_p, d2f_p;
+    unsigned int m, n, ell, iter, n_roots;
+    double r, theta, min, temp;
+
+    /*  To avoid float conversion, cast _Complex_I to double precision.       */
     const complex double dbl_I = (complex double)_Complex_I;
 
+    /*  These special values are from the HSS paper.                          */
+    const double s = ceil(0.26632*log(deg));
+    const double N = 1000.0*ceil(8.32547*deg*log(deg));
+    const double factor_1 = 1.0 + sqrt(2.0);
+    const double factor_2 = (deg - 1.0) / deg;
+    const double angle_factor = 2.0*PI / N;
+
+    /*  Check if the input pointer is NULL. This will be treated as a constant*
+     *  polynomial and we will say it has zero roots.                         */
     if (roots == NULL)
         return 0U;
 
+    /*  Similarly, if the degree is zero there are no roots (ignoring the     *
+     *  zero polynomial which has infinitely many roots).                     */
     if (deg == 0U)
         return 0U;
 
-    s_real = ceil(0.26632*log(deg));
-    N_real = ceil(8.32547*deg*log(deg));
-
-    s = (unsigned int )s_real;
-    N = (unsigned int)N_real;
+    /*  Initialize the number of roots to zero.                               */
     n_roots = 0;
 
-    factor_1 = 1.0 + sqrt(2.0);
-    factor_2 = (deg - 1.0) / deg;
-    root = 0.0;
-
-
-    for (m = 0U; m < s; ++m)
+    /*  Loop over the circles specified in the HSS paper.                     */
+    for (m = 1U; m <= s; ++m)
     {
+        /*  The fundamental theorem of algebra tells us there are at most     *
+         *  deg roots. If we've found this many, we can quit.                 */
         if (n_roots >= deg)
             break;
 
-        r = factor_1 + pow(factor_2, (2*m+1)/(4*s));
+        /*  Radius of the current circle.                                     */
+        r = factor_1*pow(factor_2, (2.0*m + 1.0) / (4.0*s));
 
-        for (n=0; n<N; ++n)
+        /*  Loop over the angles specified in the HSS paper.                  */
+        for (n = 0U; n < N; ++n)
         {
+            /*  Check if we've already found deg many roots.                  */
             if (n_roots >= deg)
                 break;
 
-            theta = 2*PI*n/N;
+            /*  Compute the angle.                                            */
+            theta = angle_factor*n;
+
+            /*  The starting point for Halley's method is specified by the    *
+             *  angle and circle we're on. Compute this point.                */
             p = r * (cos(theta) + dbl_I*sin(theta));
 
+            /*  Run Halley's method on this point, see if we converge.        */
             for (iter = 0U; iter < root_finder_max_iter; ++iter)
             {
+                /*  Halley's method requires f(z), f'(z), and f''(z), unlike  *
+                 *  Newton's which doesn't need f''(z). Compute these.        */
                 f_p = poly_eval(p);
                 df_p = poly_prime_eval(p);
                 d2f_p = poly_double_prime_eval(p);
-                root = p - 2.0*f_p * df_p / (2*df_p*df_p - f_p * d2f_p);
-                if (cabs(poly_eval(root)) < 1.0e-10)
+
+                /*  Compute the next step in sequence.                        */
+                p = p - 2.0*f_p * df_p / (2*df_p*df_p - f_p * d2f_p);
+                p = p - f_p / df_p;
+
+                /*  Check for convergence.                                    */
+                if (cabs(poly_eval(p)) < 1.0E-10)
                     break;
-
-                p = root;
             }
+            /*  End of for (iter = 0U; iter < root_finder_max_iter; ++iter).  */
 
-            if (cabs(poly_eval(root)) < 1.0E-8)
+            /*  See if this new element is indeed a root.                     */
+            if (cabs(poly_eval(p)) < 1.0E-8)
             {
+                /*  If this is the first root, add it.                        */
                 if (n_roots == 0)
                 {
                     n_roots += 1;
-                    roots[0] = root;
+                    roots[0] = p;
                 }
+
+                /*  Otherwise check if this is a root we already found.       */
                 else
                 {
-                    min = cabs(root - roots[0]);
-                    for (ell=1; ell<n_roots; ++ell)
+                    min = cabs(p - roots[0]);
+                    for (ell = 1U; ell < n_roots; ++ell)
                     {
-                        temp = cabs(root - roots[ell]);
+                        temp = cabs(p - roots[ell]);
                         if (temp < min)
                             min = temp;
                     }
 
-                    if (min >= 0.000001)
+                    /*  If we're far enough away, this is a new root.         */
+                    if (min >= 1.0E-8)
                     {
-                        roots[n_roots] = root;
+                        roots[n_roots] = p;
                         n_roots += 1;
                     }
                 }
+                /*  End of if (n_roots == 0).                                 */
+
             }
+            /*  End of if (cabs(poly_eval(p)) < 1.0E-8).                      */
         }
+        /*  End of for (n = 0U; n < N; ++n).                                  */
     }
+    /*  End of for (m = 0U; m < s; ++m).                                      */
 
     return n_roots;
 }
+/*  End of get_roots.                                                         */
 
-static void color(struct color c, FILE *fp)
+/*  Function for writing a color to a PPM file.                               */
+static void write_color(struct color c, FILE *fp)
 {
     fputc(c.red, fp);
     fputc(c.green, fp);
     fputc(c.blue, fp);
 }
+/*  End of write_color.                                                       */
 
+/*  Function for drawing the Halley fractal of the input polynomial.          */
 int main(void)
 {
     /*  Declare a variable for the output file and give it write permission.  */
@@ -184,7 +282,6 @@ int main(void)
 
         /*  Green. RGB = (0, 255, 30).                                        */
         {0x00U, 0xFFU, 0x1EU},
-
 
         /*  Blue. RGB = (0, 30, 255).                                         */
         {0x00U, 0x1EU, 0xFFU},
@@ -234,7 +331,7 @@ int main(void)
     const double y_max =  1.0;
 
     /*  Factor for giving the image a gradient.                               */
-    unsigned char factor = 255/MaxIters;
+    unsigned char factor = 255/max_iters;
 
     /* Dummy variables to loop over.                                          */
     unsigned int iters, ind, n;
@@ -246,33 +343,29 @@ int main(void)
         puts("No Roots Found. Aborting.");
         return -1;
     }
-    else if (roots == NULL)
-    {
-        puts("get_roots returned NULL for roots pointer. Aborting.");
-        return -1;
-    }
 
     fp = fopen("halley_fractal.ppm", "w");
     fprintf(fp, "P6\n%d %d\n255\n", size, size);
 
-    for (y=0; y<size; ++y)
+    for (y = 0U; y < size; ++y)
     {
         z_y = y * (y_max - y_min)/(size - 1) + y_min;
         z_y = -z_y;
 
-        for (x=0; x<size; ++x)
+        for (x = 0U; x < size; ++x)
         {
             z_x = x * (x_max - x_min)/(size - 1) + x_min;
             z = z_x + dbl_I*z_y;
 
-            /*  Allow MaxIters number of iterations of Newton-Raphson.        */
-            for (iters=0; iters<MaxIters; ++iters)
+            /*  Allow max_iters number of iterations of Newton-Raphson.       */
+            for (iters = 0U; iters < max_iters; ++iters)
             {
                 /*  Perfrom Halley's Method on the polynomial f.              */
                 f_z = poly_eval(z);
                 df_z = poly_prime_eval(z);
                 d2f_z = poly_double_prime_eval(z);
-                root = z - 2.0*f_z * df_z / (2*df_z*df_z - f_z * d2f_z);
+                root = z - 2.0*f_z * df_z / (2.0*df_z*df_z - f_z * d2f_z);
+                root = z - f_z / df_z;
 
                 /*  Checks for convergence.                                   */
                 if (cabs(root - z) < 10e-10)
@@ -296,7 +389,7 @@ int main(void)
             }
 
             if (min > 0.1)
-                color(black, fp);
+                write_color(black, fp);
             else
             {
                 scale = (255.0-factor*iters)/255.0;
@@ -305,10 +398,11 @@ int main(void)
                 brightness.blue = (unsigned char)(scale * colors[ind].blue);
 
                 /*  Color the current pixel.                                  */
-                color(brightness, fp);
+                write_color(brightness, fp);
             }
         }
     }
 
     return 0;
 }
+/*  End of main.                                                              */
