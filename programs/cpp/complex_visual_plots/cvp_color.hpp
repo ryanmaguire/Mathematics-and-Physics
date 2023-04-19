411bccaaa6b3cb0d1c8d257632887ee901626f2d
ryanmaguire <rmaguire@protonmail.com>
2023-04-18 20:08:37 -0400

    Adding comments and function descriptions.

--- programs/cpp/complex_visual_plots/cvp_color.hpp
+++ programs/cpp/complex_visual_plots/cvp_color.hpp
@@ -18,7 +18,7 @@
  *  <https://www.gnu.org/licenses/>.                                          *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Provides a struct for using colors.                                   *
+ *      Provides a class for using colors.                                    *
  ******************************************************************************
  *  Author: Ryan Maguire                                                      *
  *  Date:   2023/02/28                                                        *
@@ -31,6 +31,7 @@
 /*  File data type found here.                                                */
 #include <cstdio>
 
+/*  Basic class for creating and writing to PPM files.                        */
 #include "cvp_ppm.hpp"
 
 /*  Namespace for the mini-project. "Newtonian Black Holes."                  */
@@ -94,7 +95,7 @@ cvp::color::color(unsigned char r, unsigned char g, unsigned char b)
     blue = b;
 }
 
-/*  Function for writing a color to a PPM file.                           */
+/*  Function for writing a color to a PPM file.                               */
 inline void cvp::color::write(FILE *fp) const
 {
     std::fputc(int(red), fp);
@@ -102,13 +103,13 @@ inline void cvp::color::write(FILE *fp) const
     std::fputc(int(blue), fp);
 }
 
-/*  Function for writing directly to a PPM struct.                        */
+/*  Function for writing directly to a PPM struct.                            */
 inline void cvp::color::write(cvp::ppm &PPM) const
 {
     write(PPM.fp);
 }
 
-/*  Scale a color by a positive real number. Used for darkening.          */
+/*  Scale a color by a positive real number. Used for darkening.              */
 inline cvp::color cvp::color::operator * (double t) const
 {
     const unsigned char r = static_cast<unsigned char>(t*red);
@@ -117,7 +118,7 @@ inline cvp::color cvp::color::operator * (double t) const
     return color(r, g, b);
 }
 
-/*  Scale a color by a positive real number. Used for darkening.          */
+/*  Scale a color by a positive real number. Used for darkening.              */
 inline void cvp::color::operator *= (double t)
 {
     red = static_cast<unsigned char>(t*red);
@@ -125,10 +126,10 @@ inline void cvp::color::operator *= (double t)
     blue = static_cast<unsigned char>(t*blue);
 }
 
-/*  Operator for adding colors. We take the average of the components.    */
+/*  Operator for adding colors. We take the average of the components.        */
 inline cvp::color cvp::color::operator + (const cvp::color &c) const
 {
-    /*  Cast the values to doubles and take the average, component-wise.  */
+    /*  Cast the values to doubles and take the average, component-wise.      */
     const double x =
         0.5 * (static_cast<double>(red) + (static_cast<double>(c.red)));
 
@@ -138,17 +139,17 @@ inline cvp::color cvp::color::operator + (const cvp::color &c) const
     const double z =
         0.5 * (static_cast<double>(blue)  + static_cast<double>(c.blue));
 
-    /*  Cast the double back to unsigned char's and return.               */
+    /*  Cast the double back to unsigned char's and return.                   */
     const unsigned char r = static_cast<unsigned char>(x);
     const unsigned char g = static_cast<unsigned char>(y);
     const unsigned char b = static_cast<unsigned char>(z);
     return color(r, g, b);
 }
 
-/*  Operator for adding colors. We take the average of the components.    */
+/*  Operator for adding colors. We take the average of the components.        */
 inline void cvp::color::operator += (const cvp::color &c)
 {
-    /*  Cast the values to doubles and take the average, component-wise.  */
+    /*  Cast the values to doubles and take the average, component-wise.      */
     const double x =
         0.5 * (static_cast<double>(red) + (static_cast<double>(c.red)));
 
@@ -158,41 +159,47 @@ inline void cvp::color::operator += (const cvp::color &c)
     const double z =
         0.5 * (static_cast<double>(blue)  + static_cast<double>(c.blue));
 
-    /*  Cast the double back to unsigned char's and return.               */
+    /*  Cast the double back to unsigned char's and return.                   */
     red = static_cast<unsigned char>(x);
     green = static_cast<unsigned char>(y);
     blue = static_cast<unsigned char>(z);
 }
 
+/*  Function for creating a black-to-white gradient.                          */
 inline cvp::color cvp::colors::white(double t)
 {
     unsigned char val = static_cast<unsigned char>(255.0 * t);
     return cvp::color(val, val, val);
 }
 
+/*  Returns the color black in RGB format.                                    */
 inline cvp::color cvp::colors::black(void)
 {
     return color(0x00U, 0x00U, 0x00U);
 }
 
+/*  Returns the color red, scaled by the input t (0 <= t <= 1).               */
 inline cvp::color cvp::colors::red(double t)
 {
     unsigned char val = static_cast<unsigned char>(255.0 * t);
     return cvp::color(val, 0x00U, 0x00U);
 }
 
+/*  Returns the color green, scaled by the input t (0 <= t <= 1).             */
 inline cvp::color cvp::colors::green(double t)
 {
     unsigned char val = static_cast<unsigned char>(255.0 * t);
     return color(0x00U, val, 0x00U);
 }
 
+/*  Returns the color blue, scaled by the input t (0 <= t <= 1).              */
 inline cvp::color cvp::colors::blue(double t)
 {
     unsigned char val = static_cast<unsigned char>(255.0 * t);
     return color(0x00U, 0x00U, val);
 }
 
+/*  Returns the color yellow, scaled by the input t (0 <= t <= 1).            */
 inline cvp::color cvp::colors::yellow(double t)
 {
     unsigned char val = static_cast<unsigned char>(255.0 * t);
