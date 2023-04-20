4c993309cd57557020208aa6ae7ffffe70ad8ea2
ryanmaguire <rmaguire@protonmail.com>
2023-04-20 18:17:46 -0400

    Adding more Newton Fractal routines.

--- programs/c/newton_fractals/main3.c
+++ programs/c/newton_fractals/main3.c
@@ -1,60 +1,14 @@
-#include "nf_inline.h"
-#include "nf_ppm.h"
-#include "nf_setup.h"
-#include "nf_complex.h"
-#include "nf_color.h"
-#include "nf_newton.h"
-#include "nf_complex_poly.h"
-#include "nf_real_poly.h"
-#include "nf_complex_roots.h"
-#include <stdio.h>
-
-static struct nf_complex coeffs[4] = {
-    {-1.0, 0.0},
-    {0.0, 0.0},
-    {0.0, 0.0},
-    {1.0, 0.0}
-};
-
-static const unsigned int degree = sizeof(coeffs)/sizeof(coeffs[0]) - 1U;
+#include "nf.h"
 
 int main(void)
 {
-    const struct nf_complex_poly poly = {coeffs, degree};
-    unsigned int x, y;
-    unsigned int iters, ind;
-    double scale;
-    struct nf_complex z;
-    struct nf_ppm ppm = nf_ppm_create("newton_fractal3.ppm");
-    struct nf_complex_roots *roots = nf_complex_poly_roots(&poly);
-    struct nf_color *colors = nf_complex_roots_get_colors(roots->number_of_roots);
-    nf_ppm_init(&ppm);
-    struct nf_color c;
-
-    for (y = 0U; y < nf_setup_ysize; ++y)
-    {
-        const double imag = nf_setup_ymin + nf_setup_py_factor*(double)y;
-
-        for (x = 0U; x < nf_setup_xsize; ++x)
-        {
-            z.real = nf_setup_xmin + nf_setup_px_factor*(double)x;
-            z.imag = imag;
-
-            iters = nf_newton_complex_poly(&poly, &z);
-
-            for (ind = 0U; ind < roots->number_of_roots; ++ind)
-                if (nf_complex_dist(&z, &roots->roots[ind]) < nf_setup_eps)
-                {
-                    c = colors[ind];
-                    break;
-                }
-
-            scale = (double)(nf_setup_max_iters - iters)*nf_setup_color_scale;
-            nf_color_scaleby(&c, scale);
-            nf_color_write_to_ppm(&c, &ppm);
-        }
-    }
-
-    nf_ppm_close(&ppm);
+    struct nf_complex coeffs[4] = {
+        {-1.0, 0.0},
+        {0.0, 0.0},
+        {0.0, 0.0},
+        {1.0, 0.0}
+    };
+    const char *name = "newton_fractal_z_cubed_minus_one.ppm";
+    COMPLEX_RUN(coeffs, name);
     return 0;
 }
---
+++ programs/c/newton_fractals/nf.h
@@ -0,0 +1,190 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of Mathematics-and-Physics.                             *
+ *                                                                            *
+ *  Mathematics-and-Physics is free software: you can redistribute it and/or  *
+ *  modify it under the terms of the GNU General Public License as published  *
+ *  by the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  Mathematics-and-Physics is distributed in the hope that it will be useful *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with Mathematics-and-Physics.  If not, see                          *
+ *  <https://www.gnu.org/licenses/>.                                          *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides routines for generating Newton fractals.                     *
+ ******************************************************************************
+ *  Author: Ryan Maguire                                                      *
+ *  Date:   2023/04/13                                                        *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef NF_H
+#define NF_H
+
+/*  NF_INLINE macro given here.                                               */
+#include "nf_inline.h"
+
+/*  Struct for working with colors in RGB format.                             */
+#include "nf_color.h"
+
+/*  Functions for creating colors from angles on the color wheel.             */
+#include "nf_colorers.h"
+
+/*  Struct for working with complex numbers.                                  */
+#include "nf_complex.h"
+
+/*  Struct for dealing with polynomials with complex coefficients.            */
+#include "nf_complex_poly.h"
+
+/*  Struct for storing the complex roots of a real or complex polynomial.     */
+#include "nf_complex_roots.h"
+
+/*  Routines for performing the Newton-Raphson method on functions.           */
+#include "nf_newton.h"
+
+/*  Struct for creating and writing to PPM files.                             */
+#include "nf_ppm.h"
+
+/*  Struct for working with polynomials with real coefficients.               */
+#include "nf_real_poly.h"
+
+/*  Basic setup parameters for the fractals provided here.                    */
+#include "nf_setup.h"
+
+/*  Macro for creating Newton fractals from complex polynomials.              */
+#define COMPLEX_RUN(coeffs, name)                                              \
+                                                                               \
+    /*  The degree can be computed by the length of the array minus 1.       */\
+    const unsigned int degree = sizeof(coeffs)/sizeof(coeffs[0]) - 1U;         \
+                                                                               \
+    /*  Create a polynomial from the given coefficients.                     */\
+    const struct nf_complex_poly poly = {coeffs, degree};                      \
+                                                                               \
+    /*  Indices for the x and y axes of the PPM file.                        */\
+    unsigned int x, y;                                                         \
+                                                                               \
+    /*  Number of iterations needed in Newton's method.                      */\
+    unsigned int iters;                                                        \
+                                                                               \
+    /*  Variable for indexing over the roots.                                */\
+    unsigned int ind;                                                          \
+                                                                               \
+    /*  Factor used for darkening colors based on the number iterations.     */\
+    double scale;                                                              \
+                                                                               \
+    /*  Variable for treating a pixel in the PPM as a complex number.        */\
+    struct nf_complex z;                                                       \
+                                                                               \
+    /*  Struct for the roots of the given polynomial.                        */\
+    struct nf_complex_roots *roots;                                            \
+                                                                               \
+    /*  Struct for the colors corresponding to the roots.                    */\
+    struct nf_color *colors;                                                   \
+                                                                               \
+    /*  Color for the given pixel.                                           */\
+    struct nf_color c;                                                         \
+                                                                               \
+    /*  Create a PPM file with the given name.                               */\
+    struct nf_ppm ppm = nf_ppm_create(name);                                   \
+                                                                               \
+    /*  Initialize the PPM using the default values.                         */\
+    nf_ppm_init(&ppm);                                                         \
+                                                                               \
+    /*  Check if the init function failed.                                   */\
+    if (!ppm.fp)                                                               \
+    {                                                                          \
+        puts("nf_ppm_init failed and return NULL for FILE pointer. Abort.");   \
+        return -1;                                                             \
+    }                                                                          \
+                                                                               \
+    /*  Find the roots for the polynomial.                                   */\
+    roots = nf_complex_poly_roots(&poly);                                      \
+                                                                               \
+    /*  Check if the root finding function failed.                           */\
+    if (!roots)                                                                \
+    {                                                                          \
+        /*  Close the ppm file, print an error, and return.                  */\
+        puts("nf_complex_poly_roots returned NULL. Aborting.");                \
+        nf_ppm_close(&ppm);                                                    \
+        return -1;                                                             \
+    }                                                                          \
+                                                                               \
+    /*  It's possible malloc succeeded but the algorithm failed. Check this. */\
+    else if (!roots->roots || roots->number_of_roots == 0U)                    \
+    {                                                                          \
+        /*  Close the ppm file, print an error, and return.                  */\
+        puts("nf_complex_poly_roots failed to find roots. Aborting.");         \
+        nf_ppm_close(&ppm);                                                    \
+        return -1;                                                             \
+    }                                                                          \
+                                                                               \
+    /*  Get an array of colors, one for each root.                           */\
+    colors = nf_complex_roots_get_colors(roots->number_of_roots);              \
+                                                                               \
+    /*  The previous function calls malloc. Make sure it didn't fail.        */\
+    if (!colors)                                                               \
+    {                                                                          \
+        /*  Close the ppm, print an error, and destroy the root struct.      */\
+        puts("nf_complex_roots_get_colors returned NULL. Aborting.");          \
+        nf_ppm_close(&ppm);                                                    \
+        nf_complex_roots_destroy(&roots);                                      \
+        return -1;                                                             \
+    }                                                                          \
+                                                                               \
+    /*  Loop over the y components of the PPM file.                          */\
+    for (y = 0U; y < nf_setup_ysize; ++y)                                      \
+    {                                                                          \
+        /*  Convert the y coordinate of the pixel to the imaginary part of z.*/\
+        const double imag = nf_setup_ymin + nf_setup_py_factor*(double)y;      \
+                                                                               \
+        /*  Loop over the x coordindates.                                    */\
+        for (x = 0U; x < nf_setup_xsize; ++x)                                  \
+        {                                                                      \
+            /*  Compute the real part of the complex number z from the pixel.*/\
+            const double real = nf_setup_xmin + nf_setup_px_factor*(double)x;  \
+                                                                               \
+            /*  Treat the point (real, imag) as a complex number.            */\
+            z.real = real;                                                     \
+            z.imag = imag;                                                     \
+                                                                               \
+            /*  Perform Newton's method for poly with guess z0 = z.          */\
+            iters = nf_newton_complex_poly(&poly, &z);                         \
+                                                                               \
+            /*  If Newton's method fails the default color is black.         */\
+            c = nf_black;                                                      \
+                                                                               \
+            /*  If Newton's method succeeded z should now be close to a root.*/\
+            for (ind = 0U; ind < roots->number_of_roots; ++ind)                \
+            {                                                                  \
+                /*  If this distance is small, Newton's method converged.    */\
+                if (nf_complex_dist(&z, &roots->roots[ind]) < nf_setup_eps)    \
+                {                                                              \
+                    /*  Get the color corresponding to this root.            */\
+                    c = colors[ind];                                           \
+                    break;                                                     \
+                }                                                              \
+            }                                                                  \
+                                                                               \
+            /*  Darken the color based on the number of iterations required. */\
+            scale = (double)(nf_setup_max_iters - iters)*nf_setup_color_scale; \
+            nf_color_scaleby(&c, scale);                                       \
+                                                                               \
+            /*  Lastly, write this color to the ppm file.                    */\
+            nf_color_write_to_ppm(&c, &ppm);                                   \
+        }                                                                      \
+    }                                                                          \
+                                                                               \
+    /*  Close the PPM, destroy the root struct, and free the color array.    */\
+    nf_ppm_close(&ppm);                                                        \
+    nf_complex_roots_destroy(&roots);                                          \
+    free(colors)                                                               \
+
+#endif
+/*  End of include guard.                                                     */
